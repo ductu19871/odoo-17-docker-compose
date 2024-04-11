285a7089ad2280f8c2e1a43be0bae5b4bca776d9
Duc Tu <tund@sonix.com.vn>
2024-04-11 15:51:40 +0700

    1104

--- addons/hugo17/hugocustom/models/__init__.py
+++ addons/hugo17/hugocustom/models/__init__.py
@@ -1,8 +1,8 @@
-from . import res_users
+# from . import res_users
 from . import res_partner
-from . import product_product
-from . import account_journal
-from . import account_move
+# from . import product_product
+# from . import account_journal
+# from . import account_move
 from . import sale_delivery_method
 from . import sale_order
 from . import base
--- addons/hugo17/hugocustom/models/sale_order.py
+++ addons/hugo17/hugocustom/models/sale_order.py
@@ -3,7 +3,7 @@ from odoo import api, fields, models
 class SO(models.Model):
     _inherit = 'sale.order'
 
-    quotation_term = fields.Integer(default=30)#'Hiệu lực báo giá', 
+    quotation_term = fields.Integer(default=7)#'Hiệu lực báo giá', 
     add_state = fields.Selection([('send_request','Đã gửi yêu cầu xử lý'),
                                   ('confirmed', 'Xác nhận đơn hàng'),
                                   ('stocked', 'Đã xuất kho'),
@@ -15,5 +15,14 @@ class SO(models.Model):
     advance_money = fields.Monetary(currency_field='currency_id')#Tiền cọc
     delivery_method_id = fields.Many2one('sale.delivery.method')
     note2 = fields.Text()
+    ckt = fields.Integer(default=0, string='Chuyển khoản trước')
+    delivery_interval = fields.Char(default='05~07', string='Giao hàng trong vòng')
     def _prepare_invoice(self):
         return super(SO, self.with_context(overwrite_user_warehouse_id = self.warehouse_id ))._prepare_invoice()
+    
+    def gen_dktt(self):#điều kiện thanh toán
+        if self.ckt:
+            return f'Chuyển khoản {self.ckt}% khi đặt hàng, phần còn lại sẽ thanh toán khi có thông báo giao hàng'
+        else:
+            return 'Thanh toán khi có thông báo giao hàng'
+
